33b335df1d501e931cfed993592771b61f813693
Wassim1610 <sabrwassim@gmail.com>
2026-07-03 11:34:03 +0200

    Enhance Supabase client with auth options
    Added authentication options for Supabase client.

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -4,4 +4,10 @@ import { createClient } from '@supabase/supabase-js'
 const url = 'https://ntlynxxfuitokcrkajhm.supabase.co'
 const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im50bHlueHhmdWl0b2tjcmthamhtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI4MTk0NTUsImV4cCI6MjA5ODM5NTQ1NX0.VP9pturmpODN9Eko9D_ycxReaO37a9keW6FGJR0XfdQ' 
 
-export const supabase = createClient(url, key)
+export const supabase = createClient(url, key, {
+  auth: {
+    detectSessionInUrl: true,
+    persistSession: true,
+    autoRefreshToken: true,
+  }
+})
